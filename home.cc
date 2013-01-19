5dcfd5f61913d9646733a673725466efa1e6b032
megha55 <meghasimak@gmail.com>
2013-01-19 18:56:59 +0530

    home page

--- home.cc
+++ home.cc
@@ -26,9 +26,14 @@ void Home :: BodyContent()
     
     cout << "<div id = \"content\" class = \"content\">" << endl
          << "<div class = \"right\"> "
-         << "<form name=\"input\" action=\"branchdetails.html \" method=\"post\">"
-         << "<input type=\"submit\" value=\"Start\">"
-         << "</form>"
+         << "<br>" << endl
+         << "<a href=\"branchdetails.html\">"
+         << "<img src=\"../../SeatPlan/images/start.jpg\" alt=\"start\" "
+         << " height = 100 width = 200>" << endl
+         << "</a>"
+//         << "<form name=\"input\" action=\"branchdetails.html \" method=\"post\">"
+//         << "<input type=\"submit\" value=\"Start\">"
+//         << "</form>"
          << "</div>" << endl
          << "</div>" << endl
          << "</div>" << endl;
