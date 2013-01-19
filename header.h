e396a4c8692e428b380fb4b504fad165845da90c
megha55 <meghasimak@gmail.com>
2013-01-19 18:56:33 +0530

    header.h

--- header.h
+++ header.h
@@ -1,3 +1,20 @@
 #include <iostream>
 
+// file handling
+#include <fstream>
+
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <sstream>
+#include <vector>  
+
+// file names
+#include "file-names.h"
+
+#include "cgicc.h"
+
 using namespace std;
+
+const int MIN_SIZE = 100;
+const int MAX_SIXE = 200;
